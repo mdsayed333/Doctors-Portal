21635e847e6baa43702cdaf24bdbe89dab9ba41d
Sayed <mdsayedgv@gmail.com>
2022-05-10 04:52:26 +0600

    create react route and navbar

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#37cdbe",
+          neutral: "#3A4256",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [],
+  //...
+  plugins: [require("daisyui")],
+}
